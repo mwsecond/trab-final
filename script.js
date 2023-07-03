82891fe20d3f52745ba8da14218e3a197087e6d8
meanswar <rodrigo.a42@gmail.com>
2023-07-03 18:32:58 -0300

    trab-final-DWDM

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("hamburguer").addEventListener("click", function(){
+
+    document.querySelector(".main-nav").classList.toggle("ativado");
+    document.querySelector("#hamburguer").classList.toggle("rota");
+
+    document.querySelector(".ion-navicon-round").classList.toggle("mudacor");
+
+});
